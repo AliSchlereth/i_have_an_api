c4b1e653ba7c797da5557ab5d3eec9b9150c928f
Ali Schlereth <aschlere@sbcglobal.net>
2017-12-15 09:50:25 -0700

    Add dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject i_have_an_api "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "lets consume an api"
+  :url "https://github.com/AliSchlereth/i_have_an_api"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-http "0.6.0"]])
