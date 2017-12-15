be2bd8dc5f28d45ccd9f6cda96f7a78555a0bcb3
Ali Schlereth <aschlere@sbcglobal.net>
2017-12-15 11:03:24 -0700

    Add json dependency

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-http "0.6.0"]])
+                 [clj-http "0.6.0"]
+                 [org.clojure/data.json "0.2.6"]])
