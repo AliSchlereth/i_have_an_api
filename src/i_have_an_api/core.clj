7a520dc2633de4460b2007da42aef8ad792e1ce6
Ali Schlereth <aschlere@sbcglobal.net>
2017-12-15 10:26:27 -0700

    Setup get-endpoint for edualize

--- src/i_have_an_api/core.clj
+++ src/i_have_an_api/core.clj
@@ -1,8 +1,26 @@
-(ns i-have-an-api.core)
+(ns i-have-an-api.core
+    (:require [clj-http.client :as client]
+              [clojure.data.json :as json]))
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
 
-(defn -main [] (foo "Ali"))
+(defn extract-JSON
+  [response-body]
+  (get (json/read-str response-body) "value1"))
+
+
+(defn get-endpoint
+  "get api endpoint"
+  [endpoint]
+  (println (client/get endpoint)))
+
+(defn -main
+  []
+  (foo "Ali")
+  (get-endpoint "https://edualize.herokuapp.com/api/v1/median_expenditures/totals"))
+
+
+(-main)
